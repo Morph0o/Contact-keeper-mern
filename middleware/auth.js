1a00a0be615e5cccdfe053e9049d470239a0df92
Mickey <detrodoniv@yahoo.com>
2020-04-18 15:16:16 -0400

    fixing bugs

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 module.exports = function (req, res, next){
-    const token = res.header('x-auth-token')
+    const token = req.header('x-auth-token')
     if(!token){
         return res.status(401).json({msg:"no token auth denied"})
     }
