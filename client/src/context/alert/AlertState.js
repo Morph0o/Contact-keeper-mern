df8d884b4a3df067ca6dc4bcd1d53e499d17b488
Mickey <detrodoniv@yahoo.com>
2020-05-08 08:49:44 -0400

    testing

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react'
 import AlertContext from './alertContext'
 import alertReducer from './alertReducer'
-import uuid from 'uuid/v4'
+import uuidv4 from 'uuid/v4'
 import { 
    SET_ALERT,REMOVE_ALERT
  } from '../Types'
@@ -10,7 +10,7 @@ import {
      const initialState = []
      const [state, dispatch] = useReducer(alertReducer, initialState)
      const setAlert = (msg,type,timeout=5000)=>{
-        const id = uuid.v4()
+        const id = uuidv4()
         dispatch({
             type: SET_ALERT,
             payload: {msg,type}
